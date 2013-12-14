88e3f949668627c8441c4e260ff02e48998c6aed
mhkeller <michael.keller@gmail.com>
2013-12-14 00:20:46 -0500

    no default chart title

--- js/jquery.all-my-charts.js
+++ js/jquery.all-my-charts.js
@@ -10,8 +10,8 @@
         series: '',
         x: 'date',
         y: 'today',
-        title: 'Chart Title',
-        y_axis_label: 'Y-Axis label',
+        title: '',
+        y_axis_label: '',
         color_palette: ['#1f77b4', '#ff7f0e','#2ca02c','#d62728','#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf','#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5', '#c49c94', '#f7b6d2', '#c7c7c7', '#dbdb8d', '#9edae5'],
         min_datetick_interval: 0, // To not let the day go less than a day use 24 * 3600 * 1000
         binning: 'jenks',
